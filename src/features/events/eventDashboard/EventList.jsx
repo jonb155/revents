750ca8373d89bce24cd879c78036429303fc0a6a
jonb155 <75511848+jonb155@users.noreply.github.com>
2021-01-14 19:42:40 -0600

    End of Seciton 3

---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,14 @@
+import EventListItem from "./EventListItem";
+
+export default function EventList({events}) {
+    return (
+        <>
+            {events.map(event => (
+                <EventListItem event={event} key={event.id} />
+            ))}
+            
+           
+            
+        </>
+    )
+}
